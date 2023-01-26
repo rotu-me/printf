d7be515acb8ead7d291833a9a2f2d22c26ff4f81
rotu-me <roturugba@gmail.com>
2023-01-26 11:14:31 -0800

    Update task 0 and 1

--- _printf.c
+++ _printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
  * @format: format.
@@ -24,7 +22,7 @@ if (format[i] != '%')
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/	  
+/* write(1, &format[i], 1);*/  
 printed_chars++;
 }
 else
@@ -46,11 +44,13 @@ print_buffer(buffer, &buff_ind);
 va_end(list);
 return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
 if (*buff_ind > 0)
